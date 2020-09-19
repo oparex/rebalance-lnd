2b46d883da8af85c080a167e6a163ac816b5e5d4
Peter Opara <opara.peter@gmail.com>
2020-09-19 13:27:39 +0200

    Done

--- fee_report.py
+++ fee_report.py
@@ -45,7 +45,7 @@ class FeeReport:
             list_invoices_response = self.lnd.list_invoices(i * 100)
             i += 1
 
-            for invoice in list_invoices_response.invoices[::-1]:
+            for invoice in list_invoices_response.invoices:
                 print(invoice.creation_date, invoice.settle_date)
                 # if invoice.settled and invoice.settle_date < now - MONTH:
                 #     print(i, invoice.settle_date, now)
