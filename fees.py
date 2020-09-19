68519fb5a167fc717219012afc005cfebcb463d4
Peter Opara <opara.peter@gmail.com>
2020-09-19 11:52:42 +0200

    Added simple script to check fees minus rebalance fees paid

--- fees.py
+++ fees.py
@@ -1,8 +1,14 @@
 import json
 import os
 
-# stream = os.popen("lncli listinvoices --max_invoices 100")
 stream = os.popen("lncli feereport")
-result = json.loads(stream.read())
+fee_report = json.loads(stream.read())
 
-print(result)
+stream = os.popen("lncli listinvoices --max_invoices 100")
+invoices = json.loads(stream.read())
+
+print(invoices[0])
+print(invoices[0]["settled"])
+print(invoices[0]["settled"] is True)
+print(invoices[0]["memo"])
+print("Rebalance" in invoices[0]["memo"])
