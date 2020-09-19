84f8e95ecac30a343aed4a39fb1595aeba8b2f06
Peter Opara <opara.peter@gmail.com>
2020-09-19 12:40:50 +0200

    Work

--- fee_report.py
+++ fee_report.py
@@ -5,14 +5,22 @@ class FeeReport:
         self.lnd = lnd
 
     def generate(self):
+        report = {}
+        rebalance_invoice_payment_hashs = []
+
         feereport = self.lnd.get_feereport()
+        report["day_fee_sum"] = feereport.day_fee_sum
+        report["week_fee_sum"] = feereport.week_fee_sum
+        report["month_fee_sum"] = feereport.month_fee_sum
 
+        print(report)
 
+        invoices = self.lnd.list_invoices(0)
 
-        # invoices = self.lnd.list_invoices(0)
+        for invoice in invoices:
+            if invoice.settled and "Rebalance" in invoice.memo:
+                rebalance_invoice_payment_hashs.append(invoice.payment_hash)
 
+        print(rebalance_invoice_payment_hashs)
 
-
-        # payments = self.lnd.list_payments()
-        print(feereport)
         return True
