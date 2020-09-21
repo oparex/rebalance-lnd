c33230aebd426663e3c43073a73f4020efc37f58
Peter Opara <opara.peter@gmail.com>
2020-09-21 07:47:49 +0200

    Fixed amt_paid_sat for checking invoices

--- fee_report.py
+++ fee_report.py
@@ -100,6 +100,6 @@ class FeeReport:
                     loop = False
                 if invoice.settled and ("Sats Stacker" in invoice.memo or "Sarutobi" in invoice.memo) \
                         and invoice.settle_date > now - WEEK:
-                    got_paid += invoice.amt_paid
+                    got_paid += invoice.amt_paid_sat
 
         print(got_paid)
