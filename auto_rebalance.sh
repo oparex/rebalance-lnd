27c9731b3e962204097235743a2ff4d42380af9f
Peter Opara <opara.peter@gmail.com>
2020-09-24 07:56:38 +0200

    Increased rebalance fees

--- auto_rebalance.sh
+++ auto_rebalance.sh
@@ -4,38 +4,38 @@ echo "STARTING REBALANCE @ `date`"
 
 echo "Starting bitstamp -> nicehash rebalance"
 # bitstamp -> nicehash
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 709644595846447105 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 709644595846447105 -a 400000 -l 3 --max-fee-factor 50
 
 echo "Starting bitstamp -> hodlister_co rebalance"
 # bitstamp -> hodlister_co
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 710335089135648768 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 710335089135648768 -a 400000 -l 3 --max-fee-factor 50
 
 echo "Starting bitstamp -> CoinGate rebalance"
 # bitstamp -> CoinGate
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 711522561697579008 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 711522561697579008 -a 400000 -l 3 --max-fee-factor 50
 
 echo "Starting bitstamp -> lnamrkets rebalance"
 # bitstamp -> lnmarkets
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 709650093463896064 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 709650093463896064 -a 400000 -l 3 --max-fee-factor 50
 
 echo "Starting bitstamp -> walet of satoshi rebalance"
 # bitstamp -> walet of satoshi
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 711575338232512512 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 711575338232512512 -a 400000 -l 3 --max-fee-factor 50
 
 echo "Starting bitstamp -> citadel21 rebalance"
 # bitstamp -> citadel21
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 706717695977979905 -a 200000 -l 3 --max-fee-factor 15
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 706717695977979905 -a 200000 -l 3 --max-fee-factor 20
 
 echo "Starting bitstamp -> rompert rebalance"
 # bitstamp -> rompert
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 709139920050323456 -a 100000 -l 2 --max-fee-factor 7
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 709139920050323456 -a 100000 -l 2 --max-fee-factor 10
 
 echo "Starting bitstamp -> ion.radar.tech rebalance"
 # bitstamp -> ion.radar.tech
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 710299904768933889 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 710299904768933889 -a 400000 -l 3 --max-fee-factor 50
 
 echo "Starting bitstamp -> TYLERDURDEN rebalance"
 # bitstamp -> TYLERDURDEN
-python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 702520859994226689 -a 400000 -l 3 --max-fee-factor 30
+python3 /home/peter/python/src/rebalance-lnd/rebalance.py -f 710251526247022593 -t 702520859994226689 -a 400000 -l 3 --max-fee-factor 50
 
 echo "--------------------------------------- END REBALANCE @ `date` -------------------------------------------"
